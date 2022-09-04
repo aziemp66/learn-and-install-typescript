d51dafbd8247609beaf4596bd2e728c1c318429d
Azie Melza Pratama <aziemp66@gmail.com>
2022-09-04 11:07:44 +0700

    Learning Modules

---
+++ 14-modules/src/app.ts
@@ -0,0 +1,28 @@
+//classes
+import { Invoice } from "./classes/Invoice.js";
+
+const invOne = new Invoice("mario", "work on the mario website", 250);
+const invTwo = new Invoice("luigi", "work on the luigi website", 300);
+
+const invoices: Invoice[] = [];
+invoices.push(invOne);
+invoices.push(invTwo);
+
+invoices.forEach((inv) => {
+  console.log(inv.client, inv.amount, inv.format());
+});
+
+console.log(invoices);
+
+const form = document.querySelector(".new-item-form") as HTMLFormElement;
+
+const type = document.querySelector("#type") as HTMLSelectElement;
+const tofrom = document.querySelector("#tofrom") as HTMLInputElement;
+const details = document.querySelector("#details") as HTMLInputElement;
+const amount = document.querySelector("#amount") as HTMLInputElement;
+
+form.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+
+  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+});
