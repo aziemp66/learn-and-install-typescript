c9722279db310d151a249b754b36af08f38145a7
Azie Melza Pratama <aziemp66@gmail.com>
2022-09-04 10:55:29 +0700

    Access Modifier in Typescript

--- 13-public-private-and-readonly/src/app.ts
+++ 13-public-private-and-readonly/src/app.ts
@@ -1,13 +1,18 @@
 //classes
 class Invoice {
-  client: string;
-  details: string;
-  amount: number;
-  constructor(c: string, d: string, a: number) {
-    this.client = c;
-    this.details = d;
-    this.amount = a;
+  // public client: string;
+  // private details: string;
+  // readonly amount: number;
+  constructor(
+    readonly client: string,
+    private details: string,
+    public amount: number
+  ) {
+    // this.client = c;
+    // this.details = d;
+    // this.amount = a;
   }
+
   format() {
     return `${this.client} owes $${this.amount} for ${this.details}`;
   }
@@ -20,8 +25,9 @@ const invoices: Invoice[] = [];
 invoices.push(invOne);
 invoices.push(invTwo);
 
-invOne.client = "yoshi";
-invTwo.amount = 500;
+invoices.forEach((inv) => {
+  console.log(inv.client, inv.amount, inv.format());
+});
 
 console.log(invoices);
 
